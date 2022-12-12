2e49fd3bf46afa542eec5345af5889cceec981e0
nathanjesus <nathan.j.filho@gmail.com>
2022-12-12 11:33:22 -0300

    Update knight Ataque

--- dotnet/HallHeroes/Models/KnightModel.cs
+++ dotnet/HallHeroes/Models/KnightModel.cs
@@ -1,5 +1,7 @@
 ﻿using DataCore.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HallHeroes.Models
 {
@@ -12,8 +14,9 @@ namespace HallHeroes.Models
             idade = (int)knight.Idade;
             armas = (int)knight.Armas;
             atributo = knight.KeyAttribute;
-            ataque = (int)knight.Ataque;
-       
+            var attrribute = ObterValoresPropriedades(knight.Attribute, atributo);
+            var modWeapns = (int)knight.KnightWeapon.Sum(w => w.Weapon.Mod);
+            ataque = getAtaque(attrribute, modWeapns);
         }
         
 
@@ -24,5 +27,25 @@ namespace HallHeroes.Models
         public string atributo { get; set; }
         public int ataque { get; set; }
         public double Exp => Math.Floor((idade - 7) * Math.Pow(22, 1.45));
+        private int getAtaque(int keyAttr, int weaponsMods)
+        {
+            var ataque = 0;
+            if (keyAttr >= 0 && keyAttr <= 8) ataque = - 2;
+            else if (keyAttr >= 9 && keyAttr <= 10)  ataque = -1;
+            else if (keyAttr >= 11 && keyAttr <= 12) ataque = 0;
+            else if (keyAttr >= 13 && keyAttr <= 15) ataque = 1;
+            else if (keyAttr >= 16 && keyAttr <= 18) ataque = 2;
+            else if (keyAttr >= 19 && keyAttr <= 20) ataque = 3;
+            return 10 + ataque + weaponsMods;
+        }
+
+        private int ObterValoresPropriedades(object objeto, string  keyAttribute)
+        {
+            foreach (var item in objeto.GetType().GetProperties())
+            {
+                if (item.Name == keyAttribute) return Convert.ToInt32(item.GetValue(objeto));
+            }
+            return 0;
+        }
     }
 }
